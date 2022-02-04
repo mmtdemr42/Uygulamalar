da4464258f385dc7f8991c525bca9e3f6d56830c
Mehmet DEMİR <89140860+mmtdemr42@users.noreply.github.com>
2022-02-04 12:19:29 +0300

    Add files via upload

---
+++ partiOyOranı.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+import 'sayiHesaplama.dart';
+
+void main(List<String> args) {
+  rastgeleDegerUret();
+  oyOraniHesapla();
+}
+
+int aPartisi = 0;
+int bPartisi = 0;
+
+int cPartisi = 0;
+
+int? rastgeleDegerUret() {
+  sayac = 100;
+  while (sayac != 0) {
+    int random = Random().nextInt(3);
+    if (random == 0) {
+      aPartisi += 1;
+    } else if (random == 1) {
+      bPartisi += 1;
+    } else if (random == 2) {
+      cPartisi += 1;
+    }
+    sayac--;
+  }
+}
+
+double? oyOraniHesapla() {
+  print("A partisi oy oranı: %${(aPartisi / 100) * 100}");
+  print("B partisi oy oranı: %${(bPartisi / 100) * 100}");
+  print("C partisi oy oranı: %${(cPartisi / 100) * 100}");
+}
